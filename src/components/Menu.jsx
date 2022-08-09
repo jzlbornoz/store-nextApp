41931846e009dd038a0021da8a7d334cf81f26ae
jzlbornoz <javieraap10@gmail.com>
2022-08-09 11:46:44 -0400

    Mejora en el UI del menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
 import styles from '@styles/Menu.module.scss';
+import AppContext from '@context/AppContext';
 
 const Menu = () => {
+	const { toggleMenu } = useContext(AppContext);
 	return (
 		<aside className={styles.Menu}>
 			<ul>
-				<li>
+				<li onClick={() => toggleMenu()}>
 					<Link href="/checkout" className="title">
-						My orders
+						My Orders
 					</Link>
 				</li>
-				<li>
-					<Link href="/MyAccount">My account</Link>
+				<li onClick={() => toggleMenu()}>
+					<Link href="/MyAccount">
+						My account
+					</Link>
 				</li>
-				<li>
-					<Link href="/">Sign out</Link>
+				<li onClick={() => toggleMenu()}>
+					<Link href="/">
+						Sign out
+					</Link>
 				</li>
 			</ul>
 		</aside>
